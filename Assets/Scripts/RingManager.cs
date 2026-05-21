2704eaefeb119642526418d2bf53b56ae5bf014c
mydoot <buaqur@gmail.com>
2026-05-20 18:11:12 -0700

    continuing work on slingshot mechanic

--- Assets/Scripts/RingManager.cs
+++ Assets/Scripts/RingManager.cs
@@ -1,9 +1,16 @@
+//using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RingManager : MonoBehaviour
 {
     private Vector3 startpoint;
+    private Vector3 currentpoint;
+
+    private float maxDrag = 10f;
     
+    [SerializeField] private Camera cam;
+
     [SerializeField] private Transform ringTransform; 
 
 
@@ -19,6 +26,18 @@ public class RingManager : MonoBehaviour
         {
             Debug.Log("click");
             startpoint = ringTransform.position;
+        }
+        if (UnityEngine.InputSystem.Mouse.current.leftButton.isPressed) // modify this to work on clicking the 3d object
+        {
+            Debug.Log("dragging");
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10f;
+            currentpoint = cam.ScreenToWorldPoint(mousePos);
+
+            Vector3 dragVector = startpoint - currentpoint;
+            dragVector = Vector3.ClampMagnitude(dragVector, maxDrag);
+
+
         }
     }
 }
